4cd09a00a579509319d5231d2025006b7f3b16d5
Rone <safou348@gmail.com>
2024-04-26 16:13:05 +0100

    edit config

--- lua/rahim/config.lua
+++ lua/rahim/config.lua
@@ -1,3 +1,14 @@
 vim.keymap.set("n", "<C-s>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
