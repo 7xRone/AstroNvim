1fe19889a0b8c2bc36a63217123f540fe72c97fd
Rone <safou348@gmail.com>
2024-05-02 10:58:24 +0100

    refactoring the eslint config

--- lua/astronvim/plugins/configs/eslint-lsp-config.lua
+++ lua/astronvim/plugins/configs/eslint-lsp-config.lua
@@ -1,11 +1,29 @@
 
-local lspconfig = require 'lspconfig'
-local eslint = require 'eslint-lsp'
 
-lspconfig.eslint.setup{
-  settings = {
-    languages = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
-    autoFixOnSave = true,
-  }
-}
+return function(_, _)
+  local setup_servers = function()
+    local eslint = {
+      lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+      lintStdin = true,
+      lintFormats = {"%f:%l:%c: %m"},
+      lintIgnoreExitCode = true,
+      formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+      formatStdin = true
+    }
+
+    local null_ls = require("null-ls")
+    null_ls.config({
+      sources = { null_ls.builtins.diagnostics.eslint.with(eslint) },
+    })
+
+    require("lspconfig")["null-ls"].setup({})
+  end
+
+  local astrocore = require "astrocore"
+  if astrocore.is_available "mason-lspconfig.nvim" then
+    astrocore.on_load("mason-lspconfig.nvim", setup_servers)
+  else
+    setup_servers()
+  end
+end
 
